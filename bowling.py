26b8d72db1a7ac3604df97041f3b10a9ba31167d
Lilla Szekesi <szekesililla93@gmail.com>
2017-11-24 15:33:24 +0100

    the calculation of roll score was clarified and MAX_FRAME constant was added

--- bowling.py
+++ bowling.py
@@ -8,26 +8,24 @@ def score(game):
         the total score (integer)
     """
 
+    game = game.lower()
     result = 0
-    frame = 1
     in_first_half = True
+    frame = 1
+    MAX_FRAMES = 10   
+
     for i in range(len(game)):
+        # calculates the score of the roll
+        result += get_value(game[i])
         if game[i] == '/':
-            result += 10 - last
-        else:
-            result += get_value(game[i])
-        # if not in_first_half:
-            # frame += 1
-        if frame < 10  and get_value(game[i]) == 10:
-            if game[i] == '/':
-                result += get_value(game[i+1])
-            elif game[i] == 'X' or game[i] == 'x':
-                result += get_value(game[i+1])
+            result += - get_value(game[i-1])
+        if frame < MAX_FRAMES and game[i] in ['x', '/']:
+            result += get_value(game[i+1])
+            if game[i] == 'x':
+                result += get_value(game[i+2])
                 if game[i+2] == '/':
-                    result += 10 - get_value(game[i+1])
-                else:
-                    result += get_value(game[i+2])
-        last = get_value(game[i])
+                    result -= get_value(game[i+1])
+
         if not in_first_half:
             frame += 1
         if in_first_half == True:
