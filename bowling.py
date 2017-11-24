7d8e37eae9df203d67873aef5323d1f04245334c
Lilla Szekesi <szekesililla93@gmail.com>
2017-11-24 15:28:59 +0100

    get_value() function was clarified

--- bowling.py
+++ bowling.py
@@ -49,15 +49,12 @@ def get_value(char):
         the score of the roll (integer)
     """
 
-    if char == '1' or char == '2' or char == '3' or \
-       char == '4' or char == '5' or char == '6' or \
-       char == '7' or char == '8' or char == '9':
+    if char >= '1' and char <= '9':
         return int(char)
-    elif char == 'X' or char == 'x':
-        return 10
-    elif char == '/':
+    elif char in ['x', '/']:
         return 10
     elif char == '-':
         return 0
     else:
         raise ValueError()
+
